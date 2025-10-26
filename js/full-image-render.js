7fa0a2a1b8d41bc2db644a97bf325f49f351c6c4
Алёна Старцева <rena_black@mail.ru>
2025-10-26 21:16:03 +0300

    объекты и функции

---
+++ js/full-image-render.js
@@ -0,0 +1,12 @@
+const bigPictureImage = document.querySelector('.big-picture__img img');
+const socialCaption = document.querySelector('.social__caption');
+const likesCount = document.querySelector('.likes-count');
+
+const renderFullImage = (photosData) => {
+  bigPictureImage.src = photosData.url;
+  socialCaption.textContent = photosData.description;
+  likesCount.textContent = photosData.likes;
+};
+
+
+export { renderFullImage };
